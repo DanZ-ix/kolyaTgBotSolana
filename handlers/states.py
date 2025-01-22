cb6eb1b1d3c3a671a9b794ca9bb7ca5bfd4764da
DanZ-ix <vpupkin@company.com>
2025-01-22 20:06:33 +0300

    added states.py

---
+++ handlers/states.py
@@ -0,0 +1,6 @@
+from aiogram.fsm.state import State, StatesGroup
+class States(StatesGroup):
+  DEFAULT_STATE = State()
+  ADD_TOKEN_STATE = State()
+  DELETE_TOKEN_STATE = State()
+  CHANGE_MARKET_CAP_STATE = State()
